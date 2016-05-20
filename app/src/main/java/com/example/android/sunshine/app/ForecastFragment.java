1b9fe119813f8f93d288cd7ce35ed155a3c126d6
Andrei Klepets <a.klepets@unlitsolutions.com>
2016-05-21 00:28:23 +0300

    3.04 populate detail text

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -106,7 +107,10 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l){
                 String forecast = mForecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                    .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+//                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -304,7 +308,6 @@ public class ForecastFragment extends Fragment {
                     }
                 }
             }
-
 
             try {
                 return getWeatherDataFromJson(forecastJsonStr, numDays);
