bb0577868b8fa17afe513d7b4b5a91e904ebd7fa
Andrei Klepets <a.klepets@unlitsolutions.com>
2016-09-30 23:20:26 +0300

    4.19 loaders

--- app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
+++ app/src/main/java/com/example/android/sunshine/app/FetchWeatherTask.java
@@ -37,8 +37,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Vector;
 
 public class FetchWeatherTask extends AsyncTask<String, Void, Void> {
